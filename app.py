2569d134af8cbcef3d02489c898bd5124d1be3c5
YagoSilvaA <yago.silva@utec.edu.pe>
2022-05-03 21:22:37 -0500

    Creando app

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 
+#Imports
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+#Configurations
+app = Flask(__name__)
+
+#Run Script
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
